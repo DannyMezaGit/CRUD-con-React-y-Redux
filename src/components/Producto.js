88a937681da944f4e56debf38be64a4125520526
Daniel Meza <danielmeza.4c@gmail.com>
2020-01-22 11:26:03 -0700

    llevar usuario a edición

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 // Redux
@@ -10,6 +10,7 @@ const Producto = ({producto}) => {
     const { nombre, precio, id } = producto;
 
     const dispatch = useDispatch();
+    const history = useHistory();
 
     // Confirmar si desea eliminar
     const confirmarProductoEliminar = id => {
@@ -29,7 +30,12 @@ const Producto = ({producto}) => {
                 // Pasarlo al action
                 dispatch( borrarProductoAction(id) );
             }
-          })
+          });
+    }
+
+    // Función que redirige de forma programada
+    const redireccionarEdicion = producto => {
+        history.push(`/productos/editar/${producto.id}`)
     }
 
     return ( 
@@ -37,7 +43,12 @@ const Producto = ({producto}) => {
             <td> {nombre} </td>
             <td><span> $ {precio} </span></td>
             <td className="acciones">
-                <Link to={`/productos/editar/${id}`} className="btn btn-primary mr-2">Editar</Link>
+                <button 
+                    type="button"
+                    className="btn btn-primary mr-2"
+                    onClick= {() => redireccionarEdicion(producto)}>
+                    Editar
+                </button>
                 <button 
                     type="button" 
                     className="btn btn-danger"
